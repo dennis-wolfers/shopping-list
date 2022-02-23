e05791a196d8183f109fcdffa8f1e43d4041ada0
Dennis Wolfers <dennis.wolfers@gmail.com>
2022-02-23 12:38:43 -0800

    added mock data

---
+++ src/list-data-mock.js
@@ -0,0 +1,28 @@
+var items = [
+  {
+    item: "1/2 & 1/2",
+    quantity: 1,
+  },
+  {
+    item: "Milk",
+    quantity: 1,
+  },
+];
+
+// Actual list from Barbara:
+// BIG sugar
+// Mac-N-Cheese
+// 1 lb. (or LESS) corn meal
+// Marinara, flavored
+// White vinegar, gallon
+// Cat litter
+// TWO cans black beans
+// Canola oil
+// Polar seltzers (grapefruit & black-cherry)
+// BIG tuna (x2)
+// 10-oz. can whole clams (any known brand)
+// 8-oz. clam juice
+// Bananas
+// Avos/Toms
+// Frozen Raspberries, UNsweetened
+// Milk & 1/2-N-1/2
